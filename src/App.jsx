4e3da23210d4e6413e6ff7ed525a9d3b2ec4eb1a
DuyKhanh16 <duykhanh16121993@gmail.com>
2024-04-01 15:06:29 +0700

    add phela

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from './page/login/Login'
+import Register from './page/register/Register'
+export default function App() {
+  return (
+    <div>
+      {/* <Login></Login> */}
+      <Register></Register>
+    </div>
+  )
+}
